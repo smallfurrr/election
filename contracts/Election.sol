a17c2121c2de9c81d5b67a9b64020d385ef1f07f
EliseChia <swedesurrender@gmail.com>
2019-10-15 14:32:41 +0800

    vote function and voter mapping

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,22 +7,37 @@ contract Election {
         string name;
         uint voteCount;
     }
-
-    // Read/write candidates
-    mapping(uint => Candidate) public candidates;
-
-    // Store candidates count
-    uint public candidatesCount;
-
-    // Adds candidates - runs when contract is migrated and deployed 
+    // Adds candidates - runs when contract is migrated and deployed
     constructor() public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
     }
+    // Read/write candidates
+    mapping(uint => Candidate) public candidates;
+    // Store candidates count
+    uint public candidatesCount;
+    
+    // Store accounts that have voted
+    mapping(address => bool) public voters; 
 
     // Adding a candidate - private function (only used within contract)
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    // Adding a vote 
+    function vote (uint _candidateId) public {
+        //require that person hasn't voted before 
+        require(!voters[msg.sender]);
+
+        //require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        //record that voter has voted
+        voters[msg.sender] = true;
+
+        //update candidate vote count
+        candidates[_candidateId].voteCount ++;
     }
 }
