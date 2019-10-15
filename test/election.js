ddc6cb1d160d20fb8339855b019de99f43a8724e
EliseChia <swedesurrender@gmail.com>
2019-10-15 15:15:51 +0800

    updated tests

--- test/election.js
+++ test/election.js
@@ -36,6 +36,9 @@ contract("Election", function(accounts) {
           candidateId = 1;
           return electionInstance.vote(candidateId, { from: accounts[0] });
         }).then(function(receipt) {
+          assert.equal(receipt.logs.length, 1, "an event was triggered");
+          assert.equal(receipt.logs[0].event, "votedEvent", "the event type is correct");
+          assert.equal(receipt.logs[0].args._candidateId.toNumber(), candidateId, "the candidate id is correct");
           return electionInstance.voters(accounts[0]);
         }).then(function(voted) {
           assert(voted, "the voter was marked as voted");
@@ -46,7 +49,7 @@ contract("Election", function(accounts) {
         })
       });
 
-      it("throws an exception for invalid candidates", function() {
+    it("throws an exception for invalid candidates", function() {
         return Election.deployed().then(function(instance) {
           electionInstance = instance;
           return electionInstance.vote(99, { from: accounts[1] })
@@ -62,9 +65,9 @@ contract("Election", function(accounts) {
           var voteCount = candidate2[2];
           assert.equal(voteCount, 0, "candidate 2 did not receive any votes");
         });
-      });
+    });
 
-      it("throws an exception for double voting", function() {
+    it("throws an exception for double voting", function() {
         return Election.deployed().then(function(instance) {
           electionInstance = instance;
           candidateId = 2;
@@ -86,5 +89,5 @@ contract("Election", function(accounts) {
           var voteCount = candidate2[2];
           assert.equal(voteCount, 1, "candidate 2 did not receive any votes");
         });
-      });
+    });
 });
