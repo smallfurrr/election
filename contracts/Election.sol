3caf4b960126f269b9d48799f5b01dd15ce384c6
EliseChia <swedesurrender@gmail.com>
2019-10-15 15:13:52 +0800

    ability to vote

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,43 +1,50 @@
 pragma solidity ^0.5.8;
 
 contract Election {
-    // Model a candidate
+    // Model a Candidate
     struct Candidate {
         uint id;
         string name;
         uint voteCount;
     }
-    // Adds candidates - runs when contract is migrated and deployed
-    constructor() public {
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    constructor () public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
     }
-    // Read/write candidates
-    mapping(uint => Candidate) public candidates;
-    // Store candidates count
-    uint public candidatesCount;
-    
-    // Store accounts that have voted
-    mapping(address => bool) public voters; 
 
-    // Adding a candidate - private function (only used within contract)
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
-    // Adding a vote 
     function vote (uint _candidateId) public {
-        //require that person hasn't voted before 
+        // require that they haven't voted before
         require(!voters[msg.sender]);
 
-        //require a valid candidate
+        // require a valid candidate
         require(_candidateId > 0 && _candidateId <= candidatesCount);
 
-        //record that voter has voted
+        // record that voter has voted
         voters[msg.sender] = true;
 
-        //update candidate vote count
+        // update candidate vote Count
         candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
     }
 }
